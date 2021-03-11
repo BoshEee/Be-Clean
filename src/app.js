570ddc82fd0411935141715e9d780340e562bdf2
BoshEee <bosheee.shwi@gmail.com>
2021-03-11 13:09:44 +0200

    adding slideshow

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
-new fullpage("#fullpage", {
-  autoScrolling: true,
-  navigation: true,
-});
+const slideContainer = document.querySelector('.slideshow');
+var index = 1;
+
+function slideshow() {
+  if (index >= 9) {
+    index = 1;
+  } else {
+    slideContainer.src = `../assets/${index}.jpg`;
+    index++;
+  }
+}
+setInterval(slideshow, 3000);
